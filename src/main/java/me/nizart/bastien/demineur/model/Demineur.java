4f85dc424c8810898070f7a9427d7ffb65c72e51
Bastien <bastien@nizart.me>
2022-05-24 18:51:51 +0200

    ajout : javadoc facade

--- src/main/java/me/nizart/bastien/demineur/model/Demineur.java
+++ src/main/java/me/nizart/bastien/demineur/model/Demineur.java
@@ -1,24 +1,50 @@
 package me.nizart.bastien.demineur.model;
 
-import me.nizart.bastien.demineur.controller.DemineurController;
-
+/**
+ * Cette classe représente un objet de type Demineur (facade du model)
+ * Il s'agit en fait de la représentation d'une partie de jeu.
+ * Partie propre à une grille et un nombre de drapeau.
+ * @author bastien
+ * @version 1.0
+ */
 public class Demineur {
+	/**
+	 * Grille de jeu.
+	 */
 	private Grille grille;
+
+	/**
+	 * Controlleur principal de l'application.
+	 */
 	private IController controller;
 
+	/**
+	 * Nombre de drapeaux posés durant la partie.
+	 */
 	private int nbDrapeaux;
 
+	/**
+	 * Méthode permettant de démarrer une partie.
+	 */
 	public void startPartie() {
 		this.nbDrapeaux= 0;
 		this.grille = new Grille();
 		controller.initGrille(grille);
 	}
 
+	/**
+	 * Méthode mettant fin à la partie.
+	 */
 	public void endPartie() {
 		this.grille.retirerTousDrapeaux();
 		this.grille.tousAfficher();
 	}
 
+	/**
+	 * Méthode vérifiant si la partie est gagnée.
+	 * La partie est gagnée si tous les drapeaux sont placés sur toutes les mines.
+	 * @return si la partie est gagnée.
+	 */
 	public boolean partieGagnee() {
 		if (nbDrapeaux < Grille.NB_MINES) {
 			return false;
@@ -42,6 +68,12 @@ public class Demineur {
 		return true;
 	}
 
+	/**
+	 * Cette méthode va révéler la case mise en paramètre.
+	 * Ainsi que les cases voisines si elles sont vides.
+	 * @param ligne ligne de la case à révéler.
+	 * @param colonne colonne de la case à révéler.
+	 */
 	public void revelerCases(int ligne, int colonne) {
 		Case caseActuelle = grille.getCase(ligne, colonne);
 		caseActuelle.setVisible(true);
@@ -55,22 +87,40 @@ public class Demineur {
 		}
 	}
 
+	/**
+	 * Ajoute un drapeau.
+	 */
 	public void ajouterDrapeau() {
 		this.nbDrapeaux++;
 	}
 
+	/**
+	 * Vérifie qu'il reste des drapeaux à placer.
+	 * @return si il reste des drapeaux à placer.
+	 */
 	public boolean drapeauDispo() {
 		return (Grille.NB_MINES - nbDrapeaux > 0);
 	}
 
+	/**
+	 * Cette méthode retourne le nombre de drapeaux restant à placer.
+	 * @return nombre de drapeaux à placer.
+	 */
 	public int getNbDrapeaux() {
 		return Grille.NB_MINES - nbDrapeaux;
 	}
 
+	/**
+	 * Méthode retirant un drapeau.
+	 */
 	public void retirerDrapeau() {
 		this.nbDrapeaux--;
 	}
 
+	/**
+	 * Setteur du controller principal du projet.
+	 * @param controller à set.
+	 */
 	public void setController(IController controller) {
 		this.controller = controller;
 	}
